81ba425208f83dba55074e5afa8d4d048ad15120
aliya ruby <aliya.aurelia@gmail>
2019-02-12 21:38:47 +0700

    menambah file nestedarray

---
+++ nestedarray.js
@@ -0,0 +1,8 @@
+function panggilNestedArray() {
+    var dataNama = [["Ab", "Ac", "Ad"],
+                    ["Bola1", "Bola2", "Bola3"],
+                    ["Rem1", "Rem2", "Rem3"]]
+    console.log(dataNama.length)
+}
+
+panggilNestedArray()
