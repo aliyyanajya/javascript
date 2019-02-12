5cd38eafcbbd016933d2359e93a29349434cf0ba
aliya ruby <aliya.aurelia@gmail>
2019-02-12 19:22:19 +0700

    menambah file function

---
+++ function.js
@@ -0,0 +1,15 @@
+function jalanSatu() {
+    console.log("Jalan Satu")
+}
+
+const jalanDua = function() {
+    console.log("jalan Dua")
+}
+
+const jalanTiga = ()=>{
+    console.log("jalan Tiga")
+}
+
+jalanSatu()
+jalanDua()
+jalanTiga()
