aecff39103e13f68f8ce932bb61fc634aa72baf0
aliya ruby <aliya.aurelia@gmail>
2019-02-12 21:06:05 +0700

    menambah file array

---
+++ array.js
@@ -0,0 +1,9 @@
+function panggilArray() {
+    var data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    var nama = ["Oma", "Ayah", "Bunda", "Kakak", "Adik"]
+
+    console.log(data.length)
+    console.log(nama.length)
+}
+
+panggilArray()
