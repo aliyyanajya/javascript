e34b0a3279d3506d4ea779cbfa8ce1102fc756e7
aliya ruby <aliya.aurelia@gmail>
2019-02-12 18:14:24 +0700

    menulis let dan var dalam fungsi

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var message = 'hello coding';
 let msg1 = "hallo";
 const msg2 = "Coding"
 
+if(true){
+    var message = 1;
+    let msg1 = 2;
+    console.log("testing", msg1)
+}
+
 console.log(message);
 console.log(msg1);
 console.log(msg2);
