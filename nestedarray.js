627f742c0183d7076f043e1e3a28011abd6766b0
aliya ruby <aliya.aurelia@gmail>
2019-02-12 21:44:07 +0700

    memanggil data nested

--- nestedarray.js
+++ nestedarray.js
@@ -3,7 +3,7 @@ function panggilNestedArray() {
                     ["Bola1", "Bola2", "Bola3"],
                     ["Rem1", "Rem2", "Rem3"]]
     console.log(dataNama.length)
-    console.log(dataNama[0])
+    console.log(dataNama[2][2])
 }
 
 panggilNestedArray()
