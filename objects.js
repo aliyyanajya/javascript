114e08ed9a963d45e2e64af8dd979487a11bddd8
aliya ruby <aliya.aurelia@gmail>
2019-02-14 13:59:26 +0700

    menambah file object

---
+++ objects.js
@@ -0,0 +1,12 @@
+function panggilObject() {
+    var mobil = {
+        type: 'Sedan',
+        harga: 200000000,
+        warna: 'Putih',
+    }
+    console.log(mobil)
+    console.log(mobil.type)
+    console.log(mobil.harga)
+}
+
+panggilObject()
